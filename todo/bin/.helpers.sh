a81166eb4747a3f2a12d1fa80e7709fdcdd1774d
John Lees-Miller <jdleesmiller@gmail.com>
2019-10-06 09:51:26 +0100

    Add a CI environment

--- todo/bin/.helpers.sh
+++ todo/bin/.helpers.sh
@@ -22,3 +22,14 @@ function docker_compose_in_env {
 function docker_compose_shared {
   docker-compose --file docker-compose.shared.yml "$@"
 }
+
+# Run docker-compose for CI (continuous integration).
+function docker_compose_ci {
+  docker-compose \
+    --project-name "todo_ci" \
+    --file docker-compose.yml --file docker-compose.ci.yml \
+    "$@"
+}
+
+# Script to wait for postgres to come up.
+WAIT_FOR_PG_ISREADY="while ! pg_isready --quiet; do sleep 1; done;"
